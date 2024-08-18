96f39df3fefc3ffa7bf96294bcb11728ea82fadb
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2024-08-18 13:37:01 -0600

    fix: contacts list image flash

--- src/app/(contacts)/_components/ContactsListItem.tsx
+++ src/app/(contacts)/_components/ContactsListItem.tsx
@@ -1,7 +1,6 @@
 import { formatDate } from 'date-fns';
+import Image from 'next/image';
 import Link from 'next/link';
-
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export function ContactsListItem({
   id,
@@ -17,18 +16,27 @@ export function ContactsListItem({
   return (
     <Link
       href={`/contacts/${id}`}
-      className="flex items-center justify-between border-b px-5 py-2 last:border-none hover:bg-accent"
+      className="hover:bg-accent flex items-center justify-between border-b px-5 py-2 last:border-none"
     >
       <div className="flex items-center gap-5">
-        <Avatar className="h-12 w-12">
-          <AvatarImage className="object-cover" src={imageUrl} />
-          <AvatarFallback>{name.at(0)?.toUpperCase()}</AvatarFallback>
-        </Avatar>
+        {imageUrl ? (
+          <Image
+            className="h-16 w-16 rounded-full object-cover"
+            src={imageUrl}
+            width={128}
+            height={128}
+            alt={`${name} image`}
+          />
+        ) : (
+          <div className="bg-muted text-foreground flex h-16 w-16 items-center justify-center rounded-full">
+            {name.at(0)?.toUpperCase()}
+          </div>
+        )}
 
         <p>{name}</p>
       </div>
 
-      <p className="text-sm text-muted-foreground">
+      <p className="text-muted-foreground text-sm">
         {formatDate(new Date(lastContactDate), 'PPP, hh:mm aaa')}
       </p>
     </Link>
